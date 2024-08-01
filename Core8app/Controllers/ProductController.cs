8f11f9e0da2b119e68849f30bb599fba3091dc0e
SurajDotNetDeveloper <surajcool9623@gmail.com>
2024-08-01 06:00:05 +0530

    Add project files.

---
+++ Core8app/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core8app.Controllers
+{
+    // [Route("Ganesh/Pawar")]
+    // [Route("[controller]")]
+    [Route("Product")]
+    public class ProductController : Controller
+    {
+        
+       public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("detail/{Id}")]
+        public string detail(int Id)
+        {
+            return $" You Entered {Id}";
+        }
+        [Route("ProductCategary/{catId}")]
+        public string ProductCategary( int catId)
+
+        {
+
+            return $"All Categary Product is {catId}";
+        }
+    }
+}

2a0c1bb98dc320f20853cc8f282cb40d8c0e3aa6
SurajDotNetDeveloper <surajcool9623@gmail.com>
2024-08-02 23:47:35 +0530

    transferdata

---
+++ Core8app/Controllers/CatagaryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core8app.Controllers
+{
+    public class CatagaryController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Name"] = "Ganesh";
+            ViewBag.Email = "Ganesh.netdevloper.com";
+            TempData["FreedomFighter"] = "Bal gangadhar Tilak";
+            return View();
+        }
+
+        public IActionResult Details() {
+
+            ViewBag.FreedomFighter = TempData["FreedomFighter"];   
+
+            return View();
+        }
+    }
+}

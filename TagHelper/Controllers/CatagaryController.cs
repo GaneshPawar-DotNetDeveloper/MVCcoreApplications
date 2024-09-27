62c6b52cd5d0cfa863cf3d8f59050946773bf330
SurajDotNetDeveloper <surajcool9623@gmail.com>
2024-09-28 04:01:21 +0530

    replays html helper with tag helper

---
+++ TagHelper/Controllers/CatagaryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TagHelper.Controllers
+{
+    public class CatagaryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CatagaryList()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ TagHelper/Models/Catagory.cs
@@ -0,0 +1,8 @@
+﻿namespace TagHelper.Models
+{
+    public class Catagory
+    {
+        public int CatagaryId { get; set; }
+        public string CatagoryName { get; set; }
+    }
+}
